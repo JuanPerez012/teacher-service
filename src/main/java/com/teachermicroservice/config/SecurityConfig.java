fccec8261674ead413edae69610d1752b3056ebe
JuanPerez012 <juanfer.perez0421@gmail.com>
2025-09-24 19:08:28 -0500

    feat(student): add docker files

--- src/main/java/com/teachermicroservice/config/SecurityConfig.java
+++ src/main/java/com/teachermicroservice/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.studentmicroservice.config;
+package com.teachermicroservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
