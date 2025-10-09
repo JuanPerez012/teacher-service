cd89eadb15e14e3bcbbc69795856060f324dd962
IAndresPH <amolinaph@gmail.com>
2025-10-09 15:28:17 -0500

    refactor(teacher): improve structure and add pagination support

---
+++ src/main/java/com/teachermicroservice/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.teachermicroservice.exception.handler;
+
+import com.teachermicroservice.exception.TeacherNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(TeacherNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleStudentNotFound(TeacherNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "error", "Not Found",
+                "message", ex.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "error", "Internal Server Error",
+                "message", ex.getMessage()
+        ));
+    }
+}
