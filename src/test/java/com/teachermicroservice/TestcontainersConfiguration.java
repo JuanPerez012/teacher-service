b3f1f3cb899871f5cf5b00b4764d562a5a7de853
JuanPerez012 <juanfer.perez0421@gmail.com>
2025-10-15 11:16:04 -0500

    refactor (teacher): add configurations for compatibility with SonarQube

--- src/test/java/com/teachermicroservice/TestcontainersConfiguration.java
+++ src/test/java/com/teachermicroservice/TestcontainersConfiguration.java
@@ -3,10 +3,12 @@ package com.teachermicroservice;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Profile("tc")
 class TestcontainersConfiguration {
 
 	@Bean
